b656ddb2b29fc9a6e1e6c0107efe0e710eca0a51
amoeba17 <be.nies17@protonmail.com>
2021-05-03 09:04:57 -0400

    stuff

--- animate.js
+++ animate.js
@@ -1,43 +1,59 @@
-import {operate, isOperator} from './calculator.js';
+//import {operate, isOperator} from './calculator.js';
 
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.button');
 
-let a = 0, b = 0, operator = '+';
+let currentNumber = 0, lastNumber = 0, operator;
+
+function updateCurrentNumber() {
+    currentNumber = display.textContent;
+    // maybe: lastNumber = 0;
+}
 
 function buttonClick(clickEvent) {
+    // retrieve the button from the DOM
     const button = clickEvent.target;
+    // animate the button
     button.classList.add('pressed');
+    // get the mathmetical symbol represented by the button from its text value
     const symbol = button.textContent;
-    if (button.classList.contains('operator')) {
-        if (symbol == 'CLR') {
+
+    if (button.classList.contains('number')) { // the user is inputting a number
+        if (currentNumber == 0) {
             display.textContent = '';
-        } else if (symbol == '=') {
-            b = Number(display.textContent);
-            a = operate(a, b, operator);
-            display.textContent = a;
+        }
+        // append the number symbol to the display
+        display.textContent += symbol;
+        // store the displayed number as the current number
+        updateCurrentNumber();
+    } else if (button.classList.contains('operator')){ // the user is starting an operation
+        lastNumber = currentNumber;
+        operator = symbol;
+        display.textContent = operator;
+        currentNumber = 0;
+    } else { // the user is doing something special
+        if (symbol == '=') {
+            if (operator == undefined) {
+                lastNumber = currentNumber;
+                display.textContent = currentNumber;
+            } else {
+                display.textContent = operate(currentNumber, lastNumber, operator);
+                updateCurrentNumber();
+            }
+        } else if (symbol == 'CLR') {
+            display.textContent = '';
+            updateCurrentNumber();
+        } else if (symbol == '(-)') {
+            currentNumber = -currentNumber;
+            display.textContent = currentNumber;
         } else if (symbol == '%') {
-            a = Number(display.textContent) / 100;
-            operator = symbol;
-            display.textContent = a;
-        } else {
-            a = Number(display.textContent);
-            operator = symbol;
-            display.textContent = operator;
-        }
-    } else {
-        if (isOperator(display.textContent)) {
-            display.textContent = '';
-        }
-        if (symbol == '(-)') {
-            a = -Number(display.textContent);
-            display.textContent = a;
-        } else {
-            display.textContent += symbol;
+            currentNumber = currentNumber / 100;
+            display.textContent = currentNumber;
         }
     }
 }
 
+/*
 function keyDown(keyEvent) {
     console.log(keyEvent);
     const key = keyEvent.key;
@@ -55,10 +71,13 @@ function keyDown(keyEvent) {
             case 'Backspace':
                 keyButton = document.getElementById('clear-button');
                 break;
+            default:
+                break;
         }
     }
     keyButton.click();
 }
+*/
 
 buttons.forEach(button => {
     button.addEventListener('transitionend', () => {
@@ -67,4 +86,4 @@ buttons.forEach(button => {
     button.addEventListener('click', buttonClick);
 });
 
-document.addEventListener('keydown', keyDown);
+//document.addEventListener('keydown', keyDown);
---
+++ script.js
@@ -0,0 +1,151 @@
+/* 
+Defines the mathematical operations necessary for the calculator app. 
+*/
+
+// operation functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        return Infinity;
+    } else if (b == Infinity) {
+        return 0;
+    } else {
+        return a / b;
+    }
+}
+
+function pow(b, e) {
+    let result = 1;
+    const operation = (e >= 0) ? multiply : divide;
+    e = Math.abs(e);
+    for (let i = 0; i < e; i++) {
+        result = operation(result, b);
+    }
+    return result;
+}
+
+// operate functions
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '':
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        case '^':
+            return pow(a, b);
+        default:
+            return NaN;
+    }
+}
+
+// check button text methods
+function isDigit(char) {
+    const digit = Number(char);
+    if (digit >= 0 && digit <= 9) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function isOperator(char) {
+    return char == '+' || char == '-' || char == '*' || char == '/' || char == '^';
+}
+
+function isSpecial(char) {
+    return char == 'CLR' || char == '=' || char == '%' || char == '(-)';
+}
+
+
+// DOM constants
+const display = document.getElementById('display');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const specialButtons = document.querySelectorAll('.special');
+
+// DOM functions
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', numberClick);
+});
+
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', operatorClick);
+});
+
+specialButtons.forEach(specialButton => {
+    specialButton.addEventListener('click', specialClick);
+});
+
+
+// Calculator data
+let storedValue = 0;
+let currentValue = 0;
+let operator = '';
+
+// Calculator functions
+function numberClick(clickEvent) {
+    const digit = clickEvent.target.textContent;
+    if (currentValue == 0) {
+        display.textContent = digit;
+    } else {
+        display.textContent += digit;
+    }
+    currentValue = Number(display.textContent);
+}
+
+function operatorClick(clickEvent) {
+    const operatorSymbol = clickEvent.target.textContent;
+    display.textContent = operate(operator, storedValue, currentValue);
+    operator = operatorSymbol;
+    storedValue = Number(display.textContent);
+    currentValue = 0;
+}
+
+function specialClick(clickEvent) {
+    const specialSymbol = clickEvent.target.textContent;
+    switch (specialSymbol) {
+        case '=':
+            display.textContent = operate(operator, storedValue, currentValue);
+            currentValue = Number(display.textContent);
+            storedValue = 0;
+            operator = '';
+            break;
+        case 'CLR':
+            display.textContent = '';
+            storedValue = 0;
+            currentValue = 0;
+            operator = '';
+            break;
+        case '(-)':
+            currentValue = -currentValue;
+            display.textContent = currentValue;
+            break;
+        case '%':
+            currentValue /= 100;
+            display.textContent = currentValue;
+            break;
+    }
+}
+
+
+
+
+
